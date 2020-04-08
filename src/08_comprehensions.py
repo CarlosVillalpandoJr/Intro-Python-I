9427979b54026928a3b527069a20e994361f8f70
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-04-07 21:35:10 -0500

    on file 14

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -31,7 +31,7 @@ print(y)
 # the user entered into list x.
 
 x = input("Enter comma-separated numbers: ").split(',')
-
+print(x)
 # What do you need between the square brackets to make it work?
 y = [i for i in (x) if int(i) % 2 == 0]
 print("even elements:", y) 
